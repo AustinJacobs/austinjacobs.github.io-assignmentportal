8cc9cfc4383c3a93771cc5ce92dbd16bb4b1ad00
AustinJacobs <aust.t.jac@gmail.com>
2020-10-05 17:30:15 -0600

    Finished preston page of weather site.

---
+++ lesson4/js/script.js
@@ -0,0 +1,21 @@
+function setActive() {
+    linkObj = document.getElementById('nav').getElementsByTagName('a');
+    for(i=0;i<linkObj.length;i++) { 
+      if(document.location.href.indexOf(linkObj[i].href)>=0) {
+        linkObj[i].classList.add("active");
+      }
+    }
+  }
+  
+  window.onload = setActive;
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+  
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+let d = new Date();
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+document.getElementById("current-date").innerHTML = days[d.getDay()] + ", " + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
