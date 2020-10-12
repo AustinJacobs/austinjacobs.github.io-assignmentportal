ab8f0738caf82cd6dd936e13c65445b594f45b6d
AustinJacobs <aust.t.jac@gmail.com>
2020-10-12 13:07:59 -0600

    Commented the JavaScript code

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,4 @@
+// Set the current page to active and assign an active color to it. 
 function setActive() {
     linkObj = document.getElementById('nav').getElementsByTagName('a');
     for(i=0;i<linkObj.length;i++) { 
@@ -9,15 +10,18 @@ function setActive() {
   
   window.onload = setActive;
 
+// Hamburger Button
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
   
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
+// Footer date display.
 let d = new Date();
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 document.getElementById("current-date").innerHTML = days[d.getDay()] + ", " + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
 
+// Copyright year for the footer.
 document.getElementById("copyright-year").innerHTML = (new Date().getFullYear());
