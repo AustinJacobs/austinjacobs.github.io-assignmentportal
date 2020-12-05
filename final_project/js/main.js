314b2173dc567bdfb363a4a72f43c1c4d75e0d56
AustinJacobs <aust.t.jac@gmail.com>
2020-12-05 16:02:57 -0700

    Nearly finished mobile home page of scoots

--- final_project/js/main.js
+++ final_project/js/main.js
@@ -1,4 +1,15 @@
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+// const hambutton = document.querySelector('.ham');
+// const mainnav = document.querySelector('.navigation')
   
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+// hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
