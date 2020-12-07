ab38ca0c66d68e51bf6169af266fb8e9b4fb2d01
AustinJacobs <aust.t.jac@gmail.com>
2020-12-07 16:42:16 -0700

    major progress made on tablet view

--- final_project/js/main.js
+++ final_project/js/main.js
@@ -1,7 +1,7 @@
-// const hambutton = document.querySelector('.ham');
-// const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
   
-// hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 /* Open */
 function openNav() {
@@ -13,3 +13,12 @@ function openNav() {
     document.getElementById("myNav").style.height = "0%";
   }
 
+/* Set the width of the sidebar to 250px (show it) */
+function openSideNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeSideNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
