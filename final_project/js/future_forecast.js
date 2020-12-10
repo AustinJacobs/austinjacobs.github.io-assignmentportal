251f7a1beeb3a4a73dc7182e90e7b99f1457b390
AustinJacobs <aust.t.jac@gmail.com>
2020-12-10 16:25:39 -0700

    Finished home page, and began rentals page.

---
+++ final_project/js/future_forecast.js
@@ -0,0 +1,23 @@
+// Weather API Five day forecast
+
+let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&id=";
+
+let cityIdentification = "3530103";
+
+let APIkey = "2fcc76035c8ea35fae12e45da49246aa";
+
+let requestedURL = forecastURL + cityIdentification + "&appid=" + APIkey;
+
+console.log(requestedURL)
+
+const weatherURL = requestedURL;
+fetch(weatherURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        let forecast = document.getElementById("f-temp");
+
+        forecast.textContent = Math.round(jsObject.list[37].main.temp, 0);
+        console.log(forecast)
+    });
--- final_project/js/main.js
+++ final_project/js/main.js
@@ -1,21 +1,23 @@
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
-  
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle('responsive')
+}, false);
 
 /* Open */
 function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-  }
-  
-  /* Close */
-  function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-  }
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
 
 /* Set the width of the sidebar to 250px (show it) */
 function openSideNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
+  document.getElementById("mySidepanel").style.width = "330px";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
