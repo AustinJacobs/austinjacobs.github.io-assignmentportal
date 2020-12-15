b951724b59db198b6a87e9d407dc39bb1451d3fa
AustinJacobs <aust.t.jac@gmail.com>
2020-12-15 15:57:47 -0700

    save

--- final_project/js/main.js
+++ final_project/js/main.js
@@ -1,10 +1,6 @@
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {
-  mainnav.classList.toggle('responsive')
-}, false);
-
 /* Open */
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
